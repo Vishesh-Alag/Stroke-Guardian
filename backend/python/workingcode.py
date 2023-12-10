8be4e3a53b3a7d733e195a4b13fe3cddd26e1072
Vishesh Alagh <vishesh.alag03@gmail.com>
2023-12-10 22:38:43 +0530

    frontend corrected

--- backend/python/workingcode.py
+++ backend/python/workingcode.py
@@ -146,7 +146,6 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Connect to MongoDB
-
 client = MongoClient("mongodb+srv://visheshalag03:8DSgnh73lDWRicC5@stroke-guardian.mkbxrfb.mongodb.net/")
 db = client["BrainStrokeData"]
 collection = db["cleanedBrainData2"]
